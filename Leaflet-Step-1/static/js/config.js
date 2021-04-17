300ef3fcb6f5e5968e09b8baf1f794840498aaca
kasy1tu <74841065+kasy1tu@users.noreply.github.com>
2021-04-17 10:25:22 -0700

    progress check

--- Leaflet-Step-1/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1Ijoia2FzeXR1IiwiYSI6ImNrbjZzN28yMTA2bjEycG8wZTN3dTR1a3kifQ.shHM-CizDdUIO1y0FssVSA";
---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2FzeXR1IiwiYSI6ImNrbmxhbTkzdzBnNmQybm1rZGdmOGd1dzgifQ.WmTA7CLmQQindaWNaI8L1w";
