63091265a8bcc618ba8650952ad7d706715710da
kasy1tu <74841065+kasy1tu@users.noreply.github.com>
2021-04-16 22:04:51 -0700

    progress check

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,5 +1,5 @@
 var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    attribution: 'Map data', 
     maxZoom: 18, 
     accessToken: API_KEY
 })
